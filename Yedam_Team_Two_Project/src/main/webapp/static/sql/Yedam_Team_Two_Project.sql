3b67b30748e5b0b60a60d0bbc6975b64062e27b8
admin <admin@DESKTOP-P0NRM3F>
2024-03-14 13:12:01 +0900

    SQL수정

--- Yedam_Team_Two_Project/src/main/webapp/static/sql/Yedam_Team_Two_Project.sql
+++ Yedam_Team_Two_Project/src/main/webapp/static/sql/Yedam_Team_Two_Project.sql
@@ -1,97 +1,132 @@
-﻿CREATE TABLE `Member` (
-	`member_id`	varchar(30)	NOT NULL,
-	`member_type`	varchar(20)	NULL,
-	`name`	varchar(30)	NULL,
-	`password`	varchar(30)	NULL,
-	`email`	varchar(50)	NULL,
-	`tel`	varchar(30)	NULL
+--테이블 생성
+CREATE TABLE Member(
+	member_id	NUMBER(30)	 PRIMARY KEY,
+	member_type	VARCHAR2(20),
+	name	    VARCHAR2(30),
+	password	VARCHAR2(30),
+	email	    VARCHAR2(50),
+	tel	        VARCHAR2(30)
 );
 
-CREATE TABLE `Goods` (
-	`goods_id`	number	NOT NULL,
-	`name`	varchar(100)	NULL,
-	`description`	varchar(100)	NULL,
-	`price`	number	NULL,
-	`hashtag`	varchar(100)	NULL,
-	`view_cnt`	number	NULL,
-	`image`	varchar(300)	NULL,
-	`goods_state`	varchar(20)	NULL,
-	`req_type`	varchar(20)	NULL,
-	`req_date`	date	NULL,
-	`resp_date`	date	NULL,
-	`category_id`	number	NOT NULL,
-	`member_id`	varchar(30)	NOT NULL,
-	`score`	number	NOT NULL
+CREATE TABLE Categories(
+	category_id	NUMBER       PRIMARY KEY,
+	name	    VARCHAR2(30)
 );
 
-CREATE TABLE `Categories` (
-	`category_id`	number	NOT NULL,
-	`name`	varchar(30)	NULL
+CREATE TABLE ScoreGrade(
+	score_id	NUMBER        PRIMARY KEY,
+    score       VARCHAR2(10)
 );
 
-CREATE TABLE `QnA` (
-	`qna_id`	number	NOT NULL,
-	`title`	varchar(100)	NULL,
-	`content`	varchar(1000)	NULL,
-	`date`	date	NULL	DEFAULT sysdate,
-	`member_id`	varchar(30)	NOT NULL
+CREATE TABLE Goods(
+	goods_id	NUMBER	      PRIMARY KEY,
+	name	    VARCHAR2(100),
+	description	VARCHAR2(100),
+	price	    NUMBER,
+	hashtag  	VARCHAR2(100),
+	view_cnt	NUMBER,
+	image	    VARCHAR2(300),
+	goods_state	VARCHAR2(20),
+	req_type	VARCHAR2(20),
+	req_date	DATE           DEFAULT SYSDATE,
+	resp_date	DATE           DEFAULT SYSDATE,
+	category_id	NUMBER	       REFERENCES Categories(category_id),
+	member_id	NUMBER         REFERENCES Member(member_id),
+	score_id	NUMBER	       REFERENCES ScoreGrade(score_id)
 );
 
-CREATE TABLE `Cart` (
-	`cart_id`	number	NOT NULL,
-	`goods_id`	number	NULL,
-	`member_id`	varchar(30)	NULL
+CREATE TABLE QnA(
+	qna_id	    NUMBER          PRIMARY KEY,
+	title	    VARCHAR2(100),
+	content	    VARCHAR2(1000),
+	qna_date	DATE            DEFAULT SYSDATE,
+	member_id	NUMBER   	    REFERENCES Member(member_id)
 );
 
-CREATE TABLE `Review` (
-	`review_id`	number	NOT NULL,
-	`title`	varchar(50)	NULL,
-	`content`	varchar(300)	NULL,
-	`create_date`	date	NULL,
-	`member_id`	varchar(30)	NOT NULL,
-	`goods_id`	number	NOT NULL,
-	`score`	number	NOT NULL
+CREATE TABLE Cart(
+	cart_id	    NUMBER       PRIMARY KEY,
+	goods_id	NUMBER       REFERENCES Goods(goods_id),
+	member_id	NUMBER       REFERENCES Member(member_id)
 );
 
-CREATE TABLE `ScoreGrade` (
-	`score`	number	NOT NULL
+CREATE TABLE Review(
+	review_id	NUMBER         PRIMARY KEY,
+	title	    VARCHAR2(50),
+	content	    VARCHAR2(300),
+	create_date	DATE           DEFAULT SYSDATE,
+	member_id	NUMBER         REFERENCES Member(member_id),
+	goods_id	NUMBER	       REFERENCES Goods(goods_id),
+	score_id	NUMBER	       REFERENCES ScoreGrade(score_id)
 );
 
-CREATE TABLE `wish_list` (
-	`wish_goods_id`	number	NOT NULL,
-	`member_id`	varchar(30)	NOT NULL,
-	`goods_id2`	number	NOT NULL
+CREATE TABLE wish_list(
+	wish_goods_id	NUMBER            PRIMARY KEY,
+	member_id	    NUMBER      	  REFERENCES Member(member_id),
+	goods_id	    NUMBER	          REFERENCES Goods(goods_id)
 );
+--테이블 생성 끝
 
-ALTER TABLE `Member` ADD CONSTRAINT `PK_MEMBER` PRIMARY KEY (
-	`member_id`
-);
-
-ALTER TABLE `Goods` ADD CONSTRAINT `PK_GOODS` PRIMARY KEY (
-	`goods_id`
-);
-
-ALTER TABLE `Categories` ADD CONSTRAINT `PK_CATEGORIES` PRIMARY KEY (
-	`category_id`
-);
-
-ALTER TABLE `QnA` ADD CONSTRAINT `PK_QNA` PRIMARY KEY (
-	`qna_id`
-);
-
-ALTER TABLE `Cart` ADD CONSTRAINT `PK_CART` PRIMARY KEY (
-	`cart_id`
-);
-
-ALTER TABLE `Review` ADD CONSTRAINT `PK_REVIEW` PRIMARY KEY (
-	`review_id`
-);
-
-ALTER TABLE `ScoreGrade` ADD CONSTRAINT `PK_SCOREGRADE` PRIMARY KEY (
-	`score`
-);
-
-ALTER TABLE `wish_list` ADD CONSTRAINT `PK_WISH_LIST` PRIMARY KEY (
-	`wish_goods_id`
-);
-
+--시퀀스 생성
+CREATE SEQUENCE member_id_seq
+       INCREMENT BY 1
+       START WITH   0
+       MINVALUE     0
+       MAXVALUE     99999
+       NOCYCLE
+       NOCACHE
+       NOORDER;
+       
+CREATE SEQUENCE category_id_seq
+       INCREMENT BY 1
+       START WITH   0
+       MINVALUE     0
+       MAXVALUE     99999
+       NOCYCLE
+       NOCACHE
+       NOORDER;
+       
+CREATE SEQUENCE score_id_seq
+       INCREMENT BY 1
+       START WITH   0
+       MINVALUE     0
+       MAXVALUE     99999
+       NOCYCLE
+       NOCACHE
+       NOORDER;
+       
+CREATE SEQUENCE qna_id_seq
+       INCREMENT BY 1
+       START WITH   0
+       MINVALUE     0
+       MAXVALUE     99999
+       NOCYCLE
+       NOCACHE
+       NOORDER;
+       
+CREATE SEQUENCE cart_id_seq
+       INCREMENT BY 1
+       START WITH   0
+       MINVALUE     0
+       MAXVALUE     99999
+       NOCYCLE
+       NOCACHE
+       NOORDER;
+       
+CREATE SEQUENCE review_id_seq
+       INCREMENT BY 1
+       START WITH   0
+       MINVALUE     0
+       MAXVALUE     99999
+       NOCYCLE
+       NOCACHE
+       NOORDER;
+       
+CREATE SEQUENCE wish_goods_id_seq
+       INCREMENT BY 1
+       START WITH   0
+       MINVALUE     0
+       MAXVALUE     99999
+       NOCYCLE
+       NOCACHE
+       NOORDER;       
+--시퀀스 생성 끝       
