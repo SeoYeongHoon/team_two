4565441b9071095527b8e10dcb65b3c437146834
SeoYeongHoon <tjdudgns@naver.com>
2024-03-18 12:14:28 +0900

    request -> management

--- Yedam_Team_Two_Project/src/main/webapp/static/js/request.js
+++ Yedam_Team_Two_Project/src/main/webapp/static/js/request.js
@@ -3,16 +3,5 @@
  */
 
 
-// document.getElementById("form").onsubmit = function() {
-//     let name_info = document.querySelector('#name_info');
-//     let title_info = document.querySelector('#title_info');
-//     let content_info = document.querySelector('#content_info');
+const form = document.getElementById('form');
 
-//     let name = document.getElementById('name_info');
-//     console.log(name.value);
-
-//     const confirming = confirm("신청하시겠습니까?");
-//     console.log(confirming);
-
-//     return false;
-// }
