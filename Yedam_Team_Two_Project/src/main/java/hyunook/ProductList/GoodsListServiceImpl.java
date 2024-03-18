08ef188d3a61743c48c58bc81a428dfed88b9aa4
admin <admin@DESKTOP-IROF1IR>
2024-03-18 17:03:29 +0900

    신현욱 03월 18일 작업중

---
+++ Yedam_Team_Two_Project/src/main/java/hyunook/ProductList/GoodsListServiceImpl.java
@@ -0,0 +1,21 @@
+package hyunook.ProductList;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+
+import common.DataSource;
+import common.Goods;
+import youhwan.Service.ProductService;
+
+public class GoodsListServiceImpl implements GoodsListService{
+   SqlSession session = DataSource.getInstance().openSession(true);
+   GoodsMapper mapper = session.getMapper(GoodsMapper.class);
+   
+   //개인상품 리스트
+   public List<Goods> privateProductList(Goods goods){
+	   return mapper.privateProductList(goods);
+   }
+
+
+}
