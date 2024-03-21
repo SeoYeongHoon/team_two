1c1e3184a97e826c362f408a1cbccc694442efa5
admin <admin@DESKTOP-RJBO6NB>
2024-03-21 20:15:27 +0900

    Merge branch 'test' of https://github.com/SeoYeongHoon/team_two.git into dongwon

--- Yedam_Team_Two_Project/src/main/java/dongwon/QnA/QnAControl.java
+++ Yedam_Team_Two_Project/src/main/java/dongwon/QnA/QnAControl.java
@@ -1,5 +1,4 @@
 package dongwon.QnA;
-
 import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
@@ -10,10 +9,6 @@ import javax.servlet.http.HttpSession;
 
 import common.Control;
 
-//QnA패키지의 목적은, 웹페이지의 1:1문의에서 클라이언트가 작성한 QnA의 정보를
-//DB의 QnA테이블에 저장하기 위함이다.
-
-//MainControl은 서블릿이며, QnAControl 등은 서블릿은 아니지만 연관된 역할을 수행함
 public class QnAControl implements Control {
 	
 	//Control을 재정의하여, 요청을 받고 응답을 보내는데, 보내면서 뭘 하는지를 정의함
@@ -29,44 +24,27 @@ public class QnAControl implements Control {
 		String memberId = (String) session.getAttribute("memberId");
 		
 		//요청객체의 파라미터에서 qnaTitle의 값을 가져옴
-		String qnaTitle = req.getParameter("qnaTitle");
+		String title = req.getParameter("title");
 		
 		//요청객체의 파라미터에서 qnaContent의 값을 가져옴
-		String qnaContent = req.getParameter("qnaContent");
+		String content = req.getParameter("content");
 		
 		//가져온 위 3가지 값을 담아줄 qna 객체를 생성함
 		QnA qna = new QnA();
 		
 		//qna 클래스의 setter 메소드를 사용하여, 객체에 값을 담음 (롬복 사용)
 		qna.setMemberId(memberId);
-		qna.setTitle(qnaTitle);
-		qna.setContent(qnaContent);
-		
-		//여기까지 분석하고, 아래 QnAService svc = new QnAServiceImpl()를 해석하기 위해
-		//QnAService와 QnAServiceImpl에서 분석 시작
-		
-		//이 QnA 패키지에서 구현하고자 하는 기능(메소드)을 담은 Service 객체를 생성함.*객체를 생성해야 해당 메소드를 쓰므
-		// int qnaId; 시퀀스 값
-		// Date qnaDate; SYSDATE 값
-		HttpSession session = req.getSession();
-		String memberId = (String) session.getAttribute("memberId");
-		String title = req.getParameter("qnaTitle");
-		String content = req.getParameter("qnaContent");
-		QnA qna = new QnA();
-		qna.setMemberId(memberId);
 		qna.setTitle(title);
 		qna.setContent(content);
+		//이 QnA 패키지에서 구현하고자 하는 기능(메소드)을 담은 Service 객체를 생성함.*객체를 생성해야 해당 메소드를 쓰니까
 		QnAService svc = new QnAServiceImpl();
 		qna = svc.infoQnA(qna);
-		req.setAttribute("qna", qna);
-		
 		//qna 객체에, 기능(메소드)를 적용시킴, 단 여기서는 기능상 딱히 필요가 없을 듯.
-		//qna = svc.infoQnA(qna);
 		
 		//완성된 객체를, 요청 객체에 담아서 (+ title과 content까지 담아서.)
 		req.setAttribute("qna", qna);
-		req.setAttribute("qnaTitle", qnaTitle);
-		req.setAttribute("qnaContent", qnaContent);
+		req.setAttribute("qnaTitle", title);
+		req.setAttribute("qnaContent", content);
 
 		//이 경로에 보내겠다. * 이 경로는 관리자의 요청 처리 페이지가 되어야 한다.
 		//추후 논의하고 수정이 필요하다는 소리다.
