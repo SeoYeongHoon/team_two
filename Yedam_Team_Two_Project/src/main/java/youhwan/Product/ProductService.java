eca63cd16448b918e41ba577c17b1bc78bc3d07e
admin <admin@DESKTOP-S0L3N01>
2024-03-21 17:55:05 +0900

    03-21

--- Yedam_Team_Two_Project/src/main/java/youhwan/Product/ProductService.java
+++ Yedam_Team_Two_Project/src/main/java/youhwan/Product/ProductService.java
@@ -5,8 +5,11 @@ import java.util.List;
 import common.Goods;
 
 public interface ProductService {
-	List<Goods> ProductList(Goods goods);
-	
+	//List<Goods> ProductList(Goods goods);
+	// 굿즈 데이터
 	Goods getGoods(int gid);
-	
+	// 리뷰 데이터
+	List<Review> ProductReview(int gid);
+	// 리뷰 전체 건수
+	int totalCount(int gid);
 }
--- Yedam_Team_Two_Project/src/main/java/youhwan/Product/ReviewTotalCount.java
+++ Yedam_Team_Two_Project/src/main/java/youhwan/Product/ReviewTotalCount.java
@@ -7,24 +7,21 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import common.Control;
-import common.Goods;
 
-public class ProductControl implements Control {
-	
+public class ReviewTotalCount implements Control {
+
 	@Override
 	public void exec(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		resp.setContentType("text/json;charset=utf-8");
-		// 상세페이지 데이터 가져오기
-		String gid = "1";
+		
+		String gid = "1"; //req.getParameter("gid");
+		
 		ProductService svc = new ProductServiceImpl();
-		Goods goods = svc.getGoods(Integer.parseInt(gid));
-		req.setAttribute("goods", goods);
+		int cnt = svc.totalCount(Integer.parseInt(gid));
+		
+		resp.getWriter().print("{\"totalCount\":" + cnt + "}");
 		
 
-	    String path = "jsp/product.tiles";
-	    req.getRequestDispatcher(path).forward(req, resp);
-	    
 	}
 
 }
