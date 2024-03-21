e50179e6a2c10039c997e4c028718a2698ac69ac
admin <admin@DESKTOP-IROF1IR>
2024-03-21 17:42:02 +0900

    0321 5시 41 분 신현욱

--- Yedam_Team_Two_Project/src/main/java/hyunook/ProductList/GoodsListService.java
+++ Yedam_Team_Two_Project/src/main/java/hyunook/ProductList/GoodsListService.java
@@ -8,7 +8,7 @@ import common.Goods;
 public interface GoodsListService {
     //개인판매 리스트
 
-	List<Goods> privateProductList(String goods);
+	List<Goods> privateProductList(GoodsSearch search);
 	
 	List<Goods> goodsList();
 	Goods getGoods(int goodsId);
