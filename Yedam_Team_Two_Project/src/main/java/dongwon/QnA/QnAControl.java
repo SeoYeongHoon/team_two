d4144e1ce96bd1b4ec761edbf61b854007afb2dd
admin <admin@DESKTOP-C59RQQG>
2024-03-22 12:28:45 +0900

    Merge branch 'test' of https://github.com/SeoYeongHoon/team_two.git into dongwon

--- Yedam_Team_Two_Project/src/main/java/dongwon/QnA/QnAControl.java
+++ Yedam_Team_Two_Project/src/main/java/dongwon/QnA/QnAControl.java
@@ -14,28 +14,19 @@ public class QnAControl implements Control {
 	//Control을 재정의하여, 요청을 받고 응답을 보내는데, 보내면서 뭘 하는지를 정의함
 	@Override
 	public void exec(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// int qnaId; 시퀀스 값은 DB에서 생성함
-		// Date qnaDate; SYSDATE 값도 마찬가지
-		
 		//로그인 중인 세션의 아이디를 받아와야 하므로, 세션 객체 생성
 		HttpSession session = req.getSession();
-		
-		//세션의 아이디를 받아옴
+		//QnAControl의 역할 : 로그인 체크 -- > JSP파일에서 구현하는게 더 바람직함.
+		//Control에서 구현하려면 DB와 대조하는 SQL문도 작성하는 등 소요되는 시간이 길다.
+		//그러므로, QnA컨트롤은 그냥 내버려 두자.
 		String memberId = (String) session.getAttribute("memberId");
 		
-		//요청객체의 파라미터에서 qnaTitle의 값을 가져옴
-		String title = req.getParameter("title");
+		//센드리다이렉트 코드 추가
 		
-		//요청객체의 파라미터에서 qnaContent의 값을 가져옴
-		String content = req.getParameter("content");
-
-		QnAService svc = new QnAServiceImpl();
-		svc.insertQnA(memberId, title, content);
 		//이 경로에 보내겠다. * 이 경로는 관리자의 요청 처리 페이지가 되어야 한다.
 		String path = "jsp/qna.tiles";
 		RequestDispatcher dispatch = req.getRequestDispatcher(path);
 		dispatch.forward(req, resp);
-		//테스트 해보려 했는데, 일단 로그인 쪽이 완료가 덜 된 모양임. 나중에 모여서 테스트할때 해볼 것
 	}
 
 }
